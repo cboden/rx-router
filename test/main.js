c5903c14013755b05a663d5c5b27dd1a2c589ff1
Joseph Moniz <joseph.moniz@gmail.com>
2013-10-21 21:29:22 -0700

    created examle test file

---
+++ test/main.js
@@ -0,0 +1,36 @@
+var http   = require("http");
+var Rx     = require("rx");
+var router = require("../src/index.js");
+
+var defaultHandler = function(data) {
+    var subject = new Rx.AsyncSubject();
+    data.result = "no match found";
+    return Rx.Observable.fromArray([data]);
+};
+
+var rootHandler = function(data) {
+    var subject = new Rx.AsyncSubject();
+    data.result = "hello from root";
+    return Rx.Observable.fromArray([data]);
+};
+
+var regexHandler = function(data) {
+    var subject = new Rx.AsyncSubject();
+    data.result = "hello from regex";
+    return Rx.Observable.fromArray([data]);
+};
+
+var server  = http.createServer();
+var results = router(server, {
+    "GET": [
+        ["/",           rootHandler],
+        [/^\/test\/.+/, regexHandler]
+    ]
+}, defaultHandler);
+
+results.subscribe(function(data) {
+    data.response.writeHead(200, {"Content-Type": "text/plain"});
+    data.response.end(data.result);
+});
+
+server.listen(3000);
